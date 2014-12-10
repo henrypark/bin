a0053a9fbac4beb871dfc4c232ccd359898b045c
Henry <henry.yjpark@gmail.com>
2014-12-10 18:37:43 -0500

    basic program

---
+++ ps0.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+dob = raw_input("What's your date of birth? ")
+last_name = raw_input("And your last name? ")
+
+print("Got it. Your birthday is %s and your last name is %s.") % (dob, last_name)
